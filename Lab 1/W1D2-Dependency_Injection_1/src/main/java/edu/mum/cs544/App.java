92ae707dc441ccc20c70e60fb102dd0f103dd1fb
Zelalem Zergaw <zola145103@gmail.com>
2019-11-26 17:40:46 -0600

    2nd commit

--- Lab 1/W1D2-Dependency_Injection_1/src/main/java/edu/mum/cs544/App.java
+++ Lab 1/W1D2-Dependency_Injection_1/src/main/java/edu/mum/cs544/App.java
@@ -16,6 +16,14 @@ public class App {
 		if (product2 != null) {
 			System.out.println(product2.toString());
 		}
+		Product product3 = productService.getProduct(245);
+		if (product3 != null) {
+			System.out.println(product3.toString());
+		}
+		Product product4 = productService.getProduct(483);
+		if (product4 != null) {
+			System.out.println(product4.toString());
+		}
 		ApplicationContext context= new ClassPathXmlApplicationContext("SpringConfig.xml");
 		Greeting  greeting =context.getBean("greeting",Greeting.class);
 		greeting.sayHello();
--- Lab 1/W1D2-Dependency_Injection_1/src/main/java/edu/mum/cs544/ProductService.java
+++ Lab 1/W1D2-Dependency_Injection_1/src/main/java/edu/mum/cs544/ProductService.java
@@ -9,6 +9,8 @@ public class ProductService implements IProductService {
 		productList.add(new Product(234, "LCD TV", 895.50));
 		productList.add(new Product(239, "DVD player", 315.00));
 		productList.add(new Product(423, "Plasma TV", 992.55));
+		productList.add(new Product(245, "KEC TV", 815.32));
+		productList.add(new Product(483, "SONY TV", 452.55));
 	}
 
 	public Product getProduct(int productNumber) {
