88b71312e23e83676d94b5116e5eea5470a7afee
Zelalem Zergaw <zola145103@gmail.com>
2019-11-26 16:05:08 -0600

    lab 1 first commit

---
+++ Lab 1/W1D2-Dependency_Injection_1/src/main/java/edu/mum/cs544/Greeting.java
@@ -0,0 +1,21 @@
+package edu.mum.cs544;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Greeting {
+    @Value("Hello World")
+
+    private String greeting;
+
+    public void sayHello(){
+        System.out.println(greeting);
+    }
+    public String getGreeting(){
+        return greeting;
+    }
+    public void setGreeting(String greeting){
+        this.greeting=greeting;
+    }
+}
