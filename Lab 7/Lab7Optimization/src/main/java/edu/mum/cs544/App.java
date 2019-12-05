12c51520e0e907ac12c6ace64ba5889d0256ba37
Zelalem Zergaw <zola145103@gmail.com>
2019-12-04 20:25:54 -0600

    lab 7 1st commit

---
+++ Lab 7/Lab7Optimization/src/main/java/edu/mum/cs544/App.java
@@ -0,0 +1,36 @@
+package edu.mum.cs544;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+public class App {
+
+    private static EntityManagerFactory emf;
+
+    public static void main(String[] args) throws Exception {
+        emf = Persistence.createEntityManagerFactory("cs544");
+
+        long start = System.nanoTime();
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        TypedQuery<Owner> query = em.createQuery("from Owner", Owner.class);
+        List<Owner> ownerlist = query.getResultList();
+        for (Owner o : ownerlist) {
+            o.getPets().size();
+        }
+
+        em.getTransaction().commit();
+        em.close();
+        long stop = System.nanoTime();
+
+        // stop time
+        System.out.println("To fetch this data from the database took " + (stop - start) / 1000000 + " milliseconds.");
+        System.exit(0);
+    }
+
+}
